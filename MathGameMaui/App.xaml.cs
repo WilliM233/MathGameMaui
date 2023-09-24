59a38ed1ce82c35e7eb7e7e3a31bed90f83cd2f3
WilliM233 <30969699+WilliM233@users.noreply.github.com>
2023-09-23 21:58:22 -0500

    Initial Commit

---
+++ MathGameMaui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MathGameMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MathGameMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MathGameMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
